469bb7ccfdaa325681470263895eba97481fe6d7
“sumikkys” <me@sumi.link>
2026-05-09 00:07:40 +0800

    功能+异常+组件演示

--- code/src/frontend/src/views/index.ts
+++ code/src/frontend/src/views/index.ts
@@ -11,11 +11,21 @@ export const views: Record<
   login: () => import('./_builtin/login/index.vue'),
   'not-found': () => import('./_builtin/not-found/index.vue'),
   about: () => import('./about/index.vue'),
+  component_button: () => import('./component/button/index.vue'),
+  component_card: () => import('./component/card/index.vue'),
+  component_table: () => import('./component/table/index.vue'),
   dashboard_analysis: () => import('./dashboard/analysis/index.vue'),
   dashboard_workbench: () => import('./dashboard/workbench/index.vue'),
   document_naive: () => import('./document/naive/index.vue'),
   'document_project-link': () => import('./document/project-link/index.vue'),
   document_project: () => import('./document/project/index.vue'),
   document_vite: () => import('./document/vite/index.vue'),
-  document_vue: () => import('./document/vue/index.vue')
+  document_vue: () => import('./document/vue/index.vue'),
+  exception_403: () => import('./exception/403/index.vue'),
+  exception_404: () => import('./exception/404/index.vue'),
+  exception_500: () => import('./exception/500/index.vue'),
+  'function_tab-detail': () => import('./function/tab-detail/index.vue'),
+  'function_tab-multi-detail': () => import('./function/tab-multi-detail/index.vue'),
+  function_tab: () => import('./function/tab/index.vue'),
+  function_websocket: () => import('./function/websocket/index.vue')
 };
